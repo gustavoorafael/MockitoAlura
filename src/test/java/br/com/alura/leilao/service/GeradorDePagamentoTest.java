70e298e03491b0f32f449bbb3b710cb98281fb0b
Gustavo Rafael <gustavoorafael270104@gmail.com>
2023-09-25 14:22:00 -0300

    Initial commit

---
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -0,0 +1,87 @@
+package br.com.alura.leilao.service;
+
+import java.math.BigDecimal;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import br.com.alura.leilao.dao.PagamentoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Pagamento;
+import br.com.alura.leilao.model.Usuario;
+
+class GeradorDePagamentoTest {
+
+    private GeradorDePagamento gerador;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @Mock
+    private Clock clock;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks(this);
+        this.gerador = new GeradorDePagamento(pagamentoDao, clock);
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao() {
+        Leilao leilao = leilao();
+        Lance vencedor = leilao.getLanceVencedor();
+
+        LocalDate data = LocalDate.now();
+
+        Instant instant  = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
+
+        Mockito.when(clock.instant()).thenReturn(instant);
+        Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
+
+        gerador.gerarPagamento(vencedor);
+
+        Mockito.verify(pagamentoDao).salvar(captor.capture());
+
+        Pagamento pagamento = captor.getValue();
+
+        Assert.assertEquals(LocalDate.now().plusDays(1),
+                pagamento.getVencimento());
+        Assert.assertEquals(vencedor.getValor(), pagamento.getValor());
+        Assert.assertFalse(pagamento.getPago());
+        Assert.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
+        Assert.assertEquals(leilao, pagamento.getLeilao());
+    }
+//        recuperar um objeto que está sendo criado dentro da classe a ser testada.
+//        Existe um conceito no Mockito chamado Captor, para capturar um determinado objeto.
+//        Isso que faremos: pediremos para o Mockito capturar o objeto criado e nos devolver no teste,
+//        para fazermos as assertivas e verificar se ele foi criado da maneira correta.
+
+    private Leilao leilao() {
+        Leilao leilao = new Leilao("Celular",
+                new BigDecimal("500"),
+                new Usuario("Fulano"));
+
+        Lance lance = new Lance(new Usuario("Ciclano"),
+                new BigDecimal("900"));
+
+        leilao.propoe(lance);
+        leilao.setLanceVencedor(lance);
+
+        return leilao;
+    }
+
+}
